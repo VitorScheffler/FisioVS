eee1e7674ef420083b1f365f1c6ec3b96bc059c8
Vitor Scheffler <160888109+VitorScheffler@users.noreply.github.com>
2026-02-23 19:37:57 +0000

    Alterações visuais

--- assets/js/scrip.js
+++ assets/js/scrip.js
@@ -1,10 +1,6 @@
-const menuBtn = document.getElementById('menu-btn');
+const btn = document.getElementById('menu-btn');
 const menu = document.getElementById('menu');
 
-menuBtn.addEventListener('click', () => {
-    if (menu.style.display === 'flex') {
-        menu.style.display = 'none';
-    } else {
-        menu.style.display = 'flex';
-    }
+btn.addEventListener('click', () => {
+    menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
 });
