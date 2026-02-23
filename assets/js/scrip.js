f1e27396509af3f82de6ef150f0c0f28bf587ac4
Vitor Scheffler <160888109+VitorScheffler@users.noreply.github.com>
2026-02-23 19:42:04 +0000

    Adição de animação no menu

--- assets/js/scrip.js
+++ assets/js/scrip.js
@@ -4,3 +4,20 @@ const menu = document.getElementById('menu');
 btn.addEventListener('click', () => {
     menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
 });
+
+let lastScroll = 0;
+const header = document.querySelector('.header');
+
+window.addEventListener('scroll', () => {
+    const currentScroll = window.pageYOffset;
+
+    if (currentScroll > lastScroll && currentScroll > 80) {
+        // Rolando para baixo
+        header.classList.add('hide');
+    } else {
+        // Rolando para cima
+        header.classList.remove('hide');
+    }
+
+    lastScroll = currentScroll;
+});
